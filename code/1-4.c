9ceccaf0924c52cd6c96538336fd9c4dee0baa5d
Neo6424 <129708429+Neo6424@users.noreply.github.com>
2026-04-24 09:40:36 +0900

    Update 1-4.c

--- code/1-4.c
+++ code/1-4.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
 
 int my_strcmp (const char *dst, const char *src){
-    int sum = 0;
-    while(*dst != '\0' && *src != '\0'){
-        sum = *dst - *src;
+
+    while(*dst != '\0' || *src != '\0'){
         dst++;
         src++;
-        if (*dst == '\0'){dst--;}
-        if (*src == '\0'){src--;}
     }
-    return sum;
+
+    return *dst - *src;
 }
 
 int main() {
