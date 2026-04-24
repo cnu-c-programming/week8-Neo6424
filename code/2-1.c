929bbef103d03e2310d0d53f2745b88fe89b07bf
Neo6424 <129708429+Neo6424@users.noreply.github.com>
2026-04-24 09:59:41 +0900

    Update 2-1.c

--- code/2-1.c
+++ code/2-1.c
@@ -4,11 +4,11 @@ int main() {
     char arr[] = "hello";
     char *p = "hello";
 
-    //arr[0] = "H";
-    //printf("%s\n" , arr);
+    arr[0] = 'H';
+    printf("%s\n" , arr);
 
-    p[0] = "H";
-    printf("%s\n" ,p);
+    //p[0] = "H";
+    //printf("%s\n" ,p);
 
     return 0;
 }
